338a439d5d19a4676e81fb4655b8d582eb666d63
Le Vu Hoang <rainyhoang@gmail.com>
2017-11-16 10:05:32 -0500

    done

--- liri.js
+++ liri.js
@@ -50,30 +50,18 @@ switch(action){
     console.log("{Please enter a command: my-tweets, spotify-this-song, movie-this, do-what-it-says}");
   break;
 }
+//=============================TWITTER======================================
+var twitter = require('twitter')
+function tweet(){
 
-
-// get twitt from twitter
-
-
-// var client = new twitter({
-  // consumer_key: stuffIneed.twitterKeys.consumer_key,
-  // consumer_secret: '',
-  // access_token_key: '',
-  // access_token_secret: ''
-
-  var twitter = require('twitter')
   var client = new twitter({
-    // consumer_key: stuffIneed.twitterKeys.consumer_key,
-    // consumer_secret: stuffIneed.twitterKeys.consumer_secret,
-    // access_token_key: stuffIneed.twitterKeys.access_token_key,
-    // access_token_secret: stuffIneed.twitterKeys.access_token_secret,
-    consumer_key: 'rE06R7d4t9ats8lfh9NuM93Gi',
-    consumer_secret: 'q80GkH7wT36Ob9CqfhgpYUdfbeG3UXPO6BwZ5KfYkdo2kzKgQR',
-    access_token_key: '68999431-lrsGQLB7qvE9NmtmlRme1Zr9GvTRcSLmSEMovFj7m',
-    access_token_secret: 'imXEGnwyrRpAZe3EWpLCvijNKJ4NWsNkl8oylBhjRvggH',
+    consumer_key: stuffIneed.consumer_key,
+    consumer_secret: stuffIneed.consumer_secret,
+    access_token_key: stuffIneed.access_token_key,
+    access_token_secret: stuffIneed.access_token_secret,
+
   });
 
-function tweet(){
     var params = {screen_name: "rainyhoang", count: 20};
     client.get('statuses/user_timeline', params, function(error, tweets, response) {
         if (!error) {
@@ -93,12 +81,15 @@ function tweet(){
 //
 // ---------------------------------SPOTIFY------------------------------
 
-var spotify = new Spotify({
-id: "96b5920f93df4fb8a65d963d69fe4ebd",
-secret: "263061cdddbc412eadba1822e3ca5f23"
-});
 
 function spotifySong(song){
+
+  var spotify = new Spotify({
+  id: "96b5920f93df4fb8a65d963d69fe4ebd",
+  secret: "263061cdddbc412eadba1822e3ca5f23"
+  });
+
+
     spotify.search({ type: 'track', query: song}, function(error, data){
         if(!error){
           for(var i = 0; i < data.tracks.items.length; i++){
@@ -123,18 +114,30 @@ function spotifySong(song){
 
 //============================OMDB========================================
 function OmdbData(movie){
-  var omdbURL = 'http://www.omdbapi.com/?t=' + movie + '&plot=short&tomatoes=true';
-
+  var omdbURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=40e9cece";
+console.log(omdbURL)
   request(omdbURL, function (error, response, body){
     if(!error && response.statusCode == 200){
       var body = JSON.parse(body);
 
       console.log("Title: " + body.Title);
+      console.log("-----------------------");
+
       console.log("Release Year: " + body.Year);
+      console.log("-----------------------");
+
       console.log("IMdB Rating: " + body.imdbRating);
+      console.log("-----------------------");
+
       console.log("Country: " + body.Country);
+      console.log("-----------------------");
+
       console.log("Language: " + body.Language);
+      console.log("-----------------------");
+
       console.log("Plot: " + body.Plot);
+      console.log("-----------------------");
+
       console.log("Actors: " + body.Actors);
 
     } else{
@@ -160,12 +163,6 @@ function dootherthings(){
           var dataArr = data.split(",");
           spotifySong(data[1]);
           console.log(dataArr);
+          console.log(spotifySong);
     })
     };
-
-// // //insert API key of Spotify
-
-  // var spotify = new Spotify({
-  // id: "96b5920f93df4fb8a65d963d69fe4ebd",
-  // secret: "263061cdddbc412eadba1822e3ca5f23"
-  // });
